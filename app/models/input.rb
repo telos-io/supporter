00922921e7a52464e252cc9d2f9cd0f64e5a148a
KoanBrown <koan.brown@gmail.com>
2015-06-08 11:50:39 -0700

    Added form validation

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,2 +1,7 @@
 class Input < ActiveRecord::Base
+
+  validates :name, presence: {message: "Company name must be provided"}
+
+  validates :email, presence: {message: "Email must be provided"}
+
 end
